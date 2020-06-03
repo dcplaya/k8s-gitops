939c87c8297c7e44fc5df7bfa52cfefd93448f23
Drew <dcplaya@gmail.com>
2020-06-02 18:12:32 -0700

    Updated to work with newest kubeseal

--- secrets/secrets.sh
+++ secrets/secrets.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+dry-run=client#!/usr/bin/env bash
 
 # Get Absolute Path of the base repo
 export REPO_ROOT=$(git rev-parse --show-toplevel)
@@ -69,7 +69,7 @@ do
   envsubst < "$file" \
     | \
   kubectl -n "${namespace}" create secret generic "${secret_name}-helm-values" \
-    --from-file=/dev/stdin --dry-run=true -o json \
+    --from-file=/dev/stdin --dry-run=client -o json \
     | \
   kubeseal --format=yaml --cert="${PUB_CERT}" \
     >> "${GENERATED_SECRETS}"
@@ -93,7 +93,7 @@ sed -i 's/stdin\:/values.yaml\:/g' "${GENERATED_SECRETS}"
 # NginX Basic Auth - default namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace default --dry-run=true -o json \
+  --namespace default --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -102,7 +102,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # NginX Basic Auth - kube-system namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace kube-system --dry-run=true -o json \
+  --namespace kube-system --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -111,7 +111,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # NginX Basic Auth - monitoring namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace monitoring --dry-run=true -o json \
+  --namespace monitoring --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -120,7 +120,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # NginX Basic Auth - home-automation namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace home-automation --dry-run=true -o json \
+  --namespace home-automation --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -129,7 +129,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # NginX Basic Auth - media-management namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace media-management --dry-run=true -o json \
+  --namespace media-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -138,7 +138,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # NginX Basic Auth - network-management namespace
 kubectl create secret generic nginx-basic-auth \
   --from-literal=auth="${NGINX_BASIC_AUTH}" \
-  --namespace network-management --dry-run=true -o json \
+  --namespace network-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -149,7 +149,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # Cloudflare API Key - cert-manager namespace
 kubectl create secret generic cloudflare-api-key \
   --from-literal=api-key="${CF_API_KEY}" \
-  --namespace cert-manager --dry-run=true -o json \
+  --namespace cert-manager --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -162,7 +162,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 kubectl create secret generic qbittorrent-prune \
   --from-literal=username="${QB_USERNAME}" \
   --from-literal=password="${QB_PASSWORD}" \
-  --namespace media-management --dry-run=true -o json \
+  --namespace media-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -171,7 +171,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # sonarr episode prune - media-management namespace
 kubectl create secret generic sonarr-episode-prune \
   --from-literal=api-key="${SONARR_APIKEY}" \
-  --namespace media-management --dry-run=true -o json \
+  --namespace media-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -180,7 +180,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # sonarr exporter
 kubectl create secret generic sonarr-exporter \
   --from-literal=api-key="${SONARR_APIKEY}" \
-  --namespace media-management --dry-run=true -o json \
+  --namespace media-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -189,7 +189,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # radarr exporter
 kubectl create secret generic radarr-exporter \
   --from-literal=api-key="${RADARR_APIKEY}" \
-  --namespace media-management --dry-run=true -o json \
+  --namespace media-management --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
@@ -198,7 +198,7 @@ echo "---" >> "${GENERATED_SECRETS}"
 # uptimerobot heartbeat
 kubectl create secret generic uptimerobot-heartbeat \
   --from-literal=url="${UPTIMEROBOT_HEARTBEAT_URL}" \
-  --namespace monitoring --dry-run=true -o json \
+  --namespace monitoring --dry-run=client -o json \
   | \
 kubeseal --format=yaml --cert="${PUB_CERT}" \
   >> "${GENERATED_SECRETS}"
